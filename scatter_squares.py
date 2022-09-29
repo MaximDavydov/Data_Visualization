a7aa166e8f42bbf44439a92b727c65716110fc9f
mad_proxima <maxprognet@gmail.com>
2022-09-29 19:18:49 +0300

    New scatter by dot

--- scatter_squares.py
+++ scatter_squares.py
@@ -2,19 +2,19 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-input_values = list(range(1, 6))
-squares = [num ** 2 for num in range(1, 6)]
+x_values = list(range(1, 6))
+y_values = [num ** 2 for num in x_values]
 
 plt.style.use('ggplot')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(x_values, y_values, s=200)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
-## Set size of tick labels.
-ax.tick_params(axis='both', labelsize=14)
+# Set size of tick labels.
+ax.tick_params(axis='both', which='major', labelsize=14)
 
 plt.show()
