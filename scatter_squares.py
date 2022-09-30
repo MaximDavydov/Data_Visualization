5999c672ab2103d84f6bdd7352aec46952696353
mad_proxima <maxprognet@gmail.com>
2022-09-30 15:16:05 +0300

    More train with plot and scatter

--- scatter_squares.py
+++ scatter_squares.py
@@ -2,19 +2,31 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-x_values = list(range(1, 6))
+x_values = list(range(1, 1001))
 y_values = [num ** 2 for num in x_values]
 
 plt.style.use('ggplot')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=200)
+
+# num_list_2 = list(range(0, 10000, 23))
+#
+# ax.plot(num_list_2, linewidth=3)
+ax.scatter(x_values, y_values, s=10, c=y_values, cmap=plt.cm.rainbow)
+
 
 # Set chart title and label axes.
+# Назначение заголовка диаграммы и меток осей.
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
+# Назначение размера и шрифта на осях.
 ax.tick_params(axis='both', which='major', labelsize=14)
 
+# Set the range for each axis.
+# Назначение диапазона для каждой оси.
+ax.axis([0, 1100, 0, 1100000])
+
+plt.savefig('squares_scatter.png', bbox_inches='tight')
 plt.show()
---
+++ scatter_сubes.py
@@ -0,0 +1,32 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+x_vals = list(range(1, 5000))
+y_vals = [num ** 3 for num in x_vals]
+y_vals5 = [num ** 4 for num in x_vals]
+
+# Стиль окна
+plt.style.use = 'bbplot'
+# Создание фигуры и чертежей
+fix, ax = plt.subplots()
+
+# Генерация графика точек по параметрам + цветовая палитра + размер
+ax.scatter(x_vals, y_vals, s=10, c=x_vals, cmap=plt.cm.flag)
+ax.scatter(x_vals, y_vals5, s=10, c=y_vals, cmap=plt.cm.Blues)
+
+
+# Название графика
+ax.set_title('Cubes Numbers', fontsize=24)
+# Х сторона и размер
+ax.set_xlabel('Values', fontsize=14)
+ax.set_ylabel('Cubes Values', fontsize=14)
+# Деления на осях + шрифт
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+
+# Диапазон по значениям
+ax.axis([0, 1100, 0, 11000000])
+
+
+plt.show()
