0f4ed67e3e94bb891ee1bb4f9c7cc7703eb08634
mad_proxima <maxprognet@gmail.com>
2022-10-02 16:12:00 +0300

    Ex about plot/scatter and refactor code

--- rw_visual.py
+++ rw_visual.py
@@ -4,12 +4,40 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Make a random walk.
-rw = RandomWalk(num_points=10000)
-rw.fill_walk()
+# Keep making new walks, as long as the program is active.
+while True:
+    # Make a random walk.
+    rw = RandomWalk(num_points=5000)
+    rw.fill_walk()
 
-# Plot the points in the walk.
-plt.style.use('ggplot')
-fix, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15, c=rw.x_values, cmap=plt.cm.Blues)
-plt.show()
+    # Plot the points in the walk.
+    # plt.style.use('dark_background')
+    plt.style.use('classic')
+    # Ф-ия figsize управляет размером окна, dpi - разрешение экрана
+    fix, ax = plt.subplots(figsize=(15, 7), dpi=103)
+    point_numbers = range(rw.num_point)
+
+    #zorder - номер фона по списку
+    ax.plot(rw.x_values, rw.y_values, linewidth=3, zorder=1)
+
+    # edgecolor - цвет края точек, учитывается в теме
+    # с - по каким точкам будет задаваться цвет
+    # ax.scatter(rw.x_values, rw.y_values, s=1, c=point_numbers, cmap=plt.cm.PuOr, edgecolor='none')
+    # zorder - на передний план
+
+
+    # Emphasize the first and last points.
+    ax.scatter(rw.x_values[0], rw.y_values[0], c='green', edgecolors='none', s=100, zorder=2)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100, zorder=2)
+
+
+    # Remove the axes.
+    # Удаление оси с графиков - методы ax.get_x(y)axis переводят флаг видимости
+    ax.get_xaxis().set_visible(True)
+    ax.get_yaxis().set_visible(True)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
