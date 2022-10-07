ca85822b08ee117a5b5dfafc0860aa7dd6415a5f
mad_proxima <maxprognet@gmail.com>
2022-10-07 13:15:11 +0300

    Added C-temp, death valley, try/except module

--- CSV_format/death_valley_highs_lows.py
+++ CSV_format/death_valley_highs_lows.py
@@ -1,3 +1,13 @@
+# import csv
+#
+# filename = '/Users/maxim/python_work/Data_Visualization/data/death_valley_2018_simple.csv'
+# with open(filename) as f:
+#     reader = csv.reader(f)
+#     header_row = next(reader)
+#
+#     for index, column_header in enumerate(header_row):
+#         print(index, column_header)
+
 import csv
 from datetime import datetime
 
@@ -5,7 +15,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-filename = '/Users/maxim/python_work/Data_Visualization/data/sitka_weather_2018_simple.csv'
+filename = '/Users/maxim/python_work/Data_Visualization/data/death_valley_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -16,13 +26,19 @@ with open(filename) as f:
     #     print(index, column_header)
 
     # Reading maximum value
+
+    # Get dates, and high and low temperatures from this file.
     highs, dates, lows = [], [], []
     for row in reader:
-        high, low = int(row[5]), int(row[6])
-        highs.append(((high - 32) * (5 / 9)))
-        lows.append(((low - 32) * (5 / 9)))
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        dates.append(current_date)
+        try:
+            high, low = int(row[4]), int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            highs.append(((high - 32) * (5 / 9)))
+            lows.append(((low - 32) * (5 / 9)))
+            dates.append(current_date)
 
 # print(highs)
 
@@ -37,11 +53,11 @@ plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-plt.title('Daily high and low temperatures – 2018', fontsize=24)
+plt.title('Daily high and low temperatures – 2018\nDeath Valley, CA', fontsize=20)
 plt.xlabel('', fontsize=16)
 # Вывод метки дат по диагонали
 fig.autofmt_xdate()
-plt.ylabel('Temperature (С)', fontsize=16)
+plt.ylabel('Temperature (C)', fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
 plt.show()
