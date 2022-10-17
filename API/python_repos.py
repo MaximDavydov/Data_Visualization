fae5ccf9e6cdec05b30badce3a2eab99ccf26a00
mad_proxima <maxprognet@gmail.com>
2022-10-17 19:09:37 +0300

    Some update in API Visual

--- API/python_repos.py
+++ API/python_repos.py
@@ -16,7 +16,17 @@ repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
 
 # Analysis first repository
-repo_dict = repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
-for key in sorted(repo_dict.keys()):
-    print(key)
+# repo_dict = repo_dicts[0]
+# print(f"\nKeys: {len(repo_dict)}")
+# for key in sorted(repo_dict.keys()):
+#     print(key)
+
+print("\nSelected information about each repository:")
+print("\nSelected information about first repository")
+print(f"Name: {repo_dict['name']}")
+print(f"Owner: {repo_dict['owner']['login']}")
+print(f"Stars: {repo_dict['stargazers_count']}")
+print(f"Repository: {repo_dict['html_url']}")
+print(f"Created: {repo_dict['created_at']}")
+print(f"Updated: {repo_dict['updated_at']}")
+print(f"Description: {repo_dict['description']}")
